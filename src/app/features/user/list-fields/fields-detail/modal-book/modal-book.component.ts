98d6fb11a91355cc0455094387d60ed0e7570fe6
thuongnt2 <thuongnt@edmicro.vn>
2025-03-19 10:12:47 +0700

    form book

--- src/app/features/user/list-fields/fields-detail/modal-book/modal-book.component.ts
+++ src/app/features/user/list-fields/fields-detail/modal-book/modal-book.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzModalComponent, NzModalModule } from 'ng-zorro-antd/modal';
 import { NzNotificationService } from 'ng-zorro-antd/notification';
+import { phoneNumberValidator } from '../../../../../shared/validate/check-phone-number.directive';
 
 @Component({
   selector: 'app-modal-book',
@@ -30,7 +31,10 @@ export class ModalBookComponent {
   @Output() changeVisibleBook = new EventEmitter<any>();
 
   public form: FormGroup = this.fb.group({
-
+    nameCustomer: ['', Validators.required],
+    cellPhone: ['', [Validators.required, phoneNumberValidator()]],
+    paymentMethod: ['cash', Validators.required],
+    description: ['']
   });
 
   constructor(
@@ -39,11 +43,34 @@ export class ModalBookComponent {
   ) {}
 
   handleOk(): void {
-    
+    if (this.form.invalid) {
+      Object.keys(this.form.controls).forEach(key => {
+        const control = this.form.get(key);
+        control?.markAsTouched();
+      });
+      
+      this.notification.warning(
+        'Thông báo',
+        'Vui lòng điền đầy đủ thông tin bắt buộc',
+        { nzDuration: 3000 }
+      );
+      return;
+    }
+    console.log('Form data:', this.form.value);
+    // Tiếp tục xử lý đặt sân
+    this.notification.success(
+      'Thành công',
+      'Đặt sân thành công',
+      { nzDuration: 3000 }
+    );
+    this.changeVisibleBook.emit(false);
   }
 
 
   handleCancel(): void {
+    this.form.reset({
+      paymentMethod: 'cash'
+    });
     this.changeVisibleBook.emit(false);
   }
 }
